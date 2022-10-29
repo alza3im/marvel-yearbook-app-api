ed717af0d53213561a50134ccd5ab27eb599f017
Khalil Tarabulsi <kh476379@dal.ca>
2022-10-29 20:45:41 -0300

    createda  api config file

---
+++ api/config.py
@@ -0,0 +1,6 @@
+"""Contains required secrets and configuration for apis"""
+
+BASE_URL_MARVEL = "http://gateway.marvel.com/v1/public/"
+
+SECRET_KEY = "e0ab2ebd4dd14b3a87e7853a1d5477a1622b9a77"
+PUBLIC_KEY = "7052d46a7029f867f3936d9d154e1d40"
