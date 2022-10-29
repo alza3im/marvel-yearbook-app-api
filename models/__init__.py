104c54d4be7fca6f846e9cc48cbdae5caa3e06b8
Khalil Tarabulsi <kh476379@dal.ca>
2022-10-29 20:26:20 -0300

    creating project skeleton

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+# import uuid
+
+# from flask_sqlalchemy import SQLAlchemy
+
+# db = SQLAlchemy()
+
+
+# class Character(db.Model):
+#     id = db.Column(db.String(), primary_key=True, default=lambda: str(uuid.uuid4()))
+#     name = db.Column(db.String(), uniqu=True)
+#     description = db.Column(db.String())
