fbf39343cdcb848fb394ea9ecd9c48b96e66c555
Khalil Tarabulsi <kh476379@dal.ca>
2022-10-29 20:53:48 -0300

    created /comics endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,8 @@
 from flask_restful import Api
 from api.characters.characters import Characters
+from api.comics.comics import Comics
 
 api = Api()
 
 api.add_resource(Characters, "/name")
+api.add_resource(Comics, "/comics")
