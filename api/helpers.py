dd467bb674ceadb029df0307c52883f6fe5d29cc
Khalil Tarabulsi <kh476379@dal.ca>
2022-10-29 20:48:31 -0300

    helper function for get requests

--- api/helpers.py
+++ api/helpers.py
@@ -1,6 +1,7 @@
 """Helper functions for all apis"""
 import time
 import hashlib
+import requests
 
 from api.config import (
     BASE_URL_MARVEL,
@@ -17,3 +18,12 @@ def interpolate_marvel_api(feature):
     hashmd5 = hashlib.md5(string_to_hash.encode("utf-8")).hexdigest()
     auth = f"ts={timestamp}&apikey={PUBLIC_KEY}&hash={hashmd5}&limit=20"
     return url + auth
+
+
+def get_results(url):
+    """Helper function to get results from api provided a url"""
+    resp = requests.get(url=url)
+    response_dict = resp.json()
+
+    # Todo : response.raise_for_status() implement with an exception
+    return response_dict["data"]["results"]
